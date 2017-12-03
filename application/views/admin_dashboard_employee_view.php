86320230b7b3322958101807b900b8d8a2449904
alohajaycee <alohajaycee@gmail.com>
2017-12-03 17:20:59 +0800

    employee_profile

--- application/views/admin_dashboard_employee_view.php
+++ application/views/admin_dashboard_employee_view.php
@@ -5,7 +5,8 @@ $keyword_here=null;
 if($x = $this->input->get('search')){
 	$keyword_here = $x;
 }
-$employees = $employees[0];
+
+
 
  ?>
 
@@ -14,7 +15,95 @@ $employees = $employees[0];
 		<?php  var_dump($employees); ?>
 	</div>
 </div>
-
+<div class="row top-card">
+				<img class=" img-fluid border" src="https://static.pexels.com/photos/139829/pexels-photo-139829.jpeg" style="height: 120px; width: 120px;" >
+				<div class=" ml-4">
+					<h5><strong><?php echo $employees[0]['employee_firstName'] ?></strong></h5>
+					<p class="m-0">Director, Quality Assurance Office</p>
+					<p class="accent m-0">3001</p>
+					<button type="button" class="btn mt-2 btn-post f-normal c-raduis">Edit Profile</button>
+				</div>
+		</div>
+		<div class="pl-5 pr-5">
+			<h6 class="pl-2 pr-2 f-bold"><strong>Personal Details</strong></h6>
+		</div>
+		<hr>
+		<div class="pl-5 pr-5">
+			<h6 class="pl-2 pr-2 mb-0"><strong>#4 23rd St., EBB Olongapo City</strong></h6>
+			<p  class="pl-2 pr-2"><small>Mailing Address</small></p>
+		</div>
+		<hr>
+		<div class="pl-5 pr-5">
+				<h6 class="pl-2 pr-2 mb-0"><strong>thisisemail@email.com</strong></h6>
+				<p  class="pl-2 pr-2"><small>Email</small></p>
+			</div>
+		<hr>
+		<div class="pl-5 pr-5">
+				<h6 class="pl-2 pr-2 mb-0"><strong>1234567890</strong></h6>
+				<p  class="pl-2 pr-2"><small>Home Phone</small></p>
+			</div>
+		<hr>
+		<div class="pl-5 pr-5">
+				<h6 class="pl-2 pr-2 mb-0"><strong>1234567890</strong></h6>
+				<p  class="pl-2 pr-2"><small>Mobile Number</small></p>
+			</div>
+	</div>
+	<table class="table table-hover shadow-light c-radius bg-light mt-5 border" style="margin-bottom: 100px;">
+			<thead>
+				<tr>
+					<th scope="col">Subject Code</th>
+					<th scope="col">Description</th>
+					<th scope="col">Start</th>
+					<th scope="col">End</th>
+					<th scope="col">Day Code</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td scope="row">13310123</th>
+					<td>Tapado</td>
+					<td>Gemma</td>
+					<td>College of Computer Science</td>
+					<td>Active</td>
+				</tr>
+				<tr>
+					<td scope="row">13310123</th>
+					<td>Tapado</td>
+					<td>Gemma</td>
+					<td>College of Computer Science</td>
+					<td>Active</td>
+				</tr>
+				<tr>
+					<td scope="row">13310123</th>
+					<td>Tapado</td>
+					<td>Gemma</td>
+					<td>College of Computer Science</td>
+					<td>Active</td>
+				</tr>
+				<tr>
+					<td scope="row">13310123</th>
+					<td>Tapado</td>
+					<td>Gemma</td>
+					<td>College of Computer Science</td>
+					<td>Active</td>
+				</tr>
+				<tr>
+						<td scope="row">13310123</th>
+						<td>Tapado</td>
+						<td>Gemma</td>
+						<td>College of Computer Science</td>
+						<td>Active</td>
+				</tr>
+				<tr>
+				<td scope="row">13310123</th>
+						<td>Tapado</td>
+						<td>Gemma</td>
+						<td>College of Computer Science</td>
+						<td>Active</td>
+				</tr>
+			</tbody>
+		</table>
+</div>
 
 
 
